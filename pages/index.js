4b0d11179274db74e1d0c23f0bd790b89e396a5d
Muhammad Abduh Fadila <muhammadabduh.fadila@gmail.com>
2024-11-19 05:39:54 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+    <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "177f2d909dea4142aa3e9df789554269"}'></script><!-- End Cloudflare Web Analytics -->
         </p>
       </main>
 
